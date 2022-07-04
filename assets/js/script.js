21e9e643f2329dff8b7d7e780e543651b1d59983
swvmpdad <will.g.camp@gmail.com>
2022-07-04 17:12:07 -0500

    added styles, some html, and js elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var searchFormEl = document.querySelector('#search');
+var currentWeatherEl = document.querySelector('#curr-weather');
+var futureWeatherEl = document.querySelector('#future-weather');
+
+var getWeather = function(city) {
+    
+}
