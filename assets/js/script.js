26821dfc21d6bea325b251eb253dc260d6f2aa27
swvmpdad <will.g.camp@gmail.com>
2022-07-07 18:52:26 -0500

    added select menu for state code, entered API key for geocoding, started code for conversion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var searchFormEl = document.querySelector('#search');
-var currentWeatherEl = document.querySelector('#curr-weather');
+var currentWeatherEl = document.querySelector('#current-weather');
 var futureWeatherEl = document.querySelector('#future-weather');
+var latlonApi = "b2e5f00a4ae1d8bc662c377493172660"
 
 var getWeather = function(latLon) {
 
 };
 
-var getLatlon = function(city) {
-
+var getLatlon = function(city, state) {
+    var latlonUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + state + "US&limit=1&appid=" + latlonApi;
 };
