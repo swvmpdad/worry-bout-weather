228c778470f7138a3ff62f0e9d684e1e26fd7c49
swvmpdad <will.g.camp@gmail.com>
2022-09-27 21:09:06 -0500

    changed parameters to temp, wind, humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,56 +6,53 @@ var latlonApi = "b2e5f00a4ae1d8bc662c377493172660";
 var weatherApi = "676e062b36d3433830f8fc2196aa276e"
 
 var displayWeather = function(weather) {
-    console.log(weather);
-    var currentTemp = weather.list[0].main.temp.toFixed(0);
-    var currentHigh = weather.list[0].main.temp_max.toFixed(0);
-    var currentLow = weather.list[0].main.temp_min.toFixed(0);
-    var currentConditions = weather.list[0].weather[0].description;
+    var currentTemp = weather.list[0].main.temp.toFixed(0) + " °F";
+    var currentwind = weather.list[0].wind.speed + "mph";
+    var currentHumidity = weather.list[0].main.humidity + "%";
 
     document.querySelector('#current-temp').innerHTML = currentTemp;
-    document.querySelector('#current-high').innerHTML = currentHigh;
-    document.querySelector('#current-low').innerHTML = currentLow;
-    document.querySelector('#current-conditions').innerHTML = currentConditions;
+    document.querySelector('#current-wind').innerHTML = currentwind;
+    document.querySelector('#current-humidity').innerHTML = currentHumidity;
     
-    var day1High = weather.list[8].main.temp_max.toFixed(0);
-    var day1Low = weather.list[8].main.temp_min.toFixed(0);
-    var day1Conditions = weather.list[8].weather[0].description;
+    var day1High = weather.list[8].main.temp_max.toFixed(0) + " °F";
+    var day1wind = weather.list[8].wind.speed + "mph";
+    var day1Humidity = weather.list[8].main.humidity + "%";
 
     document.querySelector('#day1-high').innerHTML = day1High;
-    document.querySelector('#day1-low').innerHTML = day1Low;
-    document.querySelector('#day1-conditions').innerHTML = day1Conditions;
+    document.querySelector('#day1-wind').innerHTML = day1wind;
+    document.querySelector('#day1-humidity').innerHTML = day1Humidity;
 
-    var day2High = weather.list[16].main.temp_max.toFixed(0);
-    var day2Low = weather.list[16].main.temp_min.toFixed(0);
-    var day2Conditions = weather.list[16].weather[0].description;
+    var day2High = weather.list[16].main.temp_max.toFixed(0) + " °F";
+    var day2wind = weather.list[16].wind.speed + "mph";
+    var day2Humidity = weather.list[16].main.humidity + "%";
 
     document.querySelector('#day2-high').innerHTML = day2High;
-    document.querySelector('#day2-low').innerHTML = day2Low;
-    document.querySelector('#day2-conditions').innerHTML = day2Conditions;
+    document.querySelector('#day2-wind').innerHTML = day2wind;
+    document.querySelector('#day2-humidity').innerHTML = day2Humidity;
 
-    var day3High = weather.list[24].main.temp_max.toFixed(0);
-    var day3Low = weather.list[24].main.temp_min.toFixed(0);
-    var day3Conditions = weather.list[24].weather[0].description;
+    var day3High = weather.list[24].main.temp_max.toFixed(0) + " °F";
+    var day3wind = weather.list[24].wind.speed + "mph";
+    var day3Humidity = weather.list[24].main.humidity + "%";
 
     document.querySelector('#day3-high').innerHTML = day3High;
-    document.querySelector('#day3-low').innerHTML = day3Low;
-    document.querySelector('#day3-conditions').innerHTML = day3Conditions;
+    document.querySelector('#day3-wind').innerHTML = day3wind;
+    document.querySelector('#day3-humidity').innerHTML = day3Humidity;
 
-    var day4High = weather.list[32].main.temp_max.toFixed(0);
-    var day4Low = weather.list[32].main.temp_min.toFixed(0);
-    var day4Conditions = weather.list[32].weather[0].description;
+    var day4High = weather.list[32].main.temp_max.toFixed(0) + " °F";
+    var day4wind = weather.list[32].wind.speed + "mph";
+    var day4Humidity = weather.list[32].main.humidity + "%";
 
     document.querySelector('#day4-high').innerHTML = day4High;
-    document.querySelector('#day4-low').innerHTML = day4Low;
-    document.querySelector('#day4-conditions').innerHTML = day4Conditions;
+    document.querySelector('#day4-wind').innerHTML = day4wind;
+    document.querySelector('#day4-humidity').innerHTML = day4Humidity;
 
-    var day5High = weather.list[39].main.temp_max.toFixed(0);
-    var day5Low = weather.list[39].main.temp_min.toFixed(0);
-    var day5Conditions = weather.list[39].weather[0].description;
+    var day5High = weather.list[39].main.temp_max.toFixed(0) + " °F";
+    var day5wind = weather.list[39].wind.speed + "mph";
+    var day5Humidity = weather.list[39].main.humidity + "%";
 
     document.querySelector('#day5-high').innerHTML = day5High;
-    document.querySelector('#day5-low').innerHTML = day5Low;
-    document.querySelector('#day5-conditions').innerHTML = day5Conditions;
+    document.querySelector('#day5-wind').innerHTML = day5wind;
+    document.querySelector('#day5-humidity').innerHTML = day5Humidity;
 
     weatherDocEl.append(currentWeatherEl);
     weatherDocEl.append(futureWeatherEl);
